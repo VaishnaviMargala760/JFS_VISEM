82b2a0f6f69bb723c5e8e34b1b6292123f9f4e00
VaishnaviMargala760 <vaishnavimargala29@gmail.com>
2026-01-08 14:01:55 +0530

    Initial commit: Maven Employee JDBC project

---
+++ EmployeeMavenJdbc_SU2/src/main/java/com/example/DBUtil.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtil {
+
+    public static Connection getConnection() throws Exception {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/skill_db",
+                "root",
+                "Vaishu__687"          // your password if any
+        );
+    }
+}
